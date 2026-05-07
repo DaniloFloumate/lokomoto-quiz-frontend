14912d6656bf4a7b06b64e09899982b408e3960b
Danilo Zdravkovic <danilo@floumate.com>
2026-05-07 20:25:36 +0200

    Set conclusion screens delay to 400ms

--- js/quiz.js
+++ js/quiz.js
@@ -372,7 +372,7 @@ const Quiz = (function() {
     document.getElementById('continueBtn').addEventListener('click', () => {
         setTimeout(() => {
             showPainRadiates();
-        }, 200);
+        }, 400);
     });
   }
 
@@ -775,7 +775,7 @@ const Quiz = (function() {
     document.getElementById('continueBtn').addEventListener('click', () => {
         setTimeout(() => {
             showAbcQuestion('pain_when', 10);
-        }, 200);
+        }, 400);
     });
   }
 
