85e36efcb99a9d8e68f02526098c4c1fd589e813
DaniloFloumate <danilo@floumate.com>
2026-05-09 11:39:40 +0200

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1286,7 +1286,7 @@ const Quiz = (function() {
         <circle cx="12" cy="12" r="6"></circle>
         <circle cx="12" cy="12" r="2"></circle>
       </svg>`,
-      spread: `<svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+      spread: `<svg width="120" height="120" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g class="spread-pulse">
           <circle cx="40" cy="40" r="34" fill="none" stroke="currentColor" stroke-width="2" opacity="0.2"/>
           <circle cx="40" cy="40" r="26" fill="none" stroke="currentColor" stroke-width="2" opacity="0.4"/>
